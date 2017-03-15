cbe13ca55731f8d31e389dc40661ff7a99266b4a
Joey Scott <joannascott29@hotmail.co.uk>
2017-03-15 17:17:09 +0000

    Refactor handler.js and router.js Relates #3

--- src/handler.js
+++ src/handler.js
@@ -3,8 +3,34 @@ var path = require ('path');
 
 var handler = module.exports = {};
 
-handler.home = function(req, res){
+handler.serveHome = function(req, res){
+  readFile(res, '/index.html')
+};
 
+const readFile = (res, endpoint) => {
+  fs.readFile(path.join(__dirname, '..', 'public', endpoint), function(err, file) {
+    if (err) {
+      console.log(err);
+      handler.notFound(null, res);
+    } else {
+      console.log(endpoint);
+      var extension = endpoint.split('.')[1];
+      var extensionType = {
+        "html": "text/html",
+        "css": "text/css",
+        "js": "application/javascript",
+        "jpg": "image/jpg",
+        "ico": "image/x-icon"
+      }
+      res.writeHead(200, { "Content-Type": extensionType[extension]});
+      res.end(file);
+    }
+  })
+}
+
+handler.servePublic = function(req, res) {
+  var endpoint = req.url;
+  readFile(res, endpoint)
 };
 
 handler.notFound = function (req, res){
--- src/router.js
+++ src/router.js
@@ -1,16 +1,13 @@
 var handler = require('./handler.js');
 
-var routes = {
-  '/': handler.home,
-  '404': handler.notFound
-}
-
 module.exports = function(req, res) {
   var endpoint = req.url;
-  if (routes[endpoint]) {
-    routes[endpoint](req, res);
-  }
-  else {
-    routes[404](req, res);
+
+  console.log('------->', endpoint);
+
+  if (endpoint === '/') {
+    handler.serveHome(req, res);
+  } else {
+    handler.servePublic(req,res);
   }
 }
