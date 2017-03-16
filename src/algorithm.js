ba91c68171ae5ba32d4ef8e9647697d662d6170a
Joey Scott <joannascott29@hotmail.co.uk>
2017-03-16 16:52:46 +0000

    Add algorithm to handle input from user Relates #43

--- src/algorithm.js
+++ src/algorithm.js
@@ -3,13 +3,28 @@ var path = require ('path');
 
 var algorithm = module.exports = {};
 
+var obj;
+fs.readFile('src/data.json','utf8', function(err,data) {
+  obj=data;
+});
+
+function getValues(obj, key) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getValues(obj[i], key));
+        } else if (i == key) {
+            objects.push(obj[i]);
+        }
+    }
+    console.log(objects);
+    return objects;
+}
+
 algorithm.serveHints = function(req, res) {
   var endpoint = req.url;
-  var obj;
-  console.log(1, 2, 3, endpoint);
-  fs.readFile('src/data.json','utf8', function(err,data) {
-    if (err) throw err;
-    obj=data;
-    });
-    console.log(obj);
-  }
+  console.log('asdfasd');
+  console.log(obj);
+  getValues(obj, 'firstname');
+}
