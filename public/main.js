697f599f49c08db1d83c0eb04ff4a2f2bc360059
Jessica Salmon <jessicasalmon05@gmail.com>
2017-03-16 10:49:20 +0000

    console log a string to check that heroku is working

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@ var userInput = '';
 function getUserInput(event){
   event.preventDefault();
   console.log(event.target.value);
+  console.log('Heroku is working - wooWooWoo');
 }
 
  document.querySelector('#form').addEventListener('keyup', getUserInput);
