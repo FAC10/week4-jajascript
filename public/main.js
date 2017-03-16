7d228f6d1378d4bd97d2a1dbd0b9b568ebfa16c6
Alexis Lui <alexislui@hotmail.co.uk>
2017-03-16 13:58:32 +0000

    Typing into the search bar now creates a URL and with the data and makes an xhr request

--- public/main.js
+++ public/main.js
@@ -1,8 +1,44 @@
 var userInput = '';
+var form = document.getElementById('form');
+
+form.addEventListener('keyup', function(e){
+fetchRequest(getDataUrl(getUserInput(e)));
+});
+
 
 function getUserInput(event){
-  event.preventDefault();
-  console.log(event.target.value);
-}
+  console.log(event);// userInput = event.target.value;
+  var key = event.key;
+  console.log(key.match(/[a-z,0-9]{1}/i));
+  if (key.match(/\w{1}/i)) {
+  // if (key.charCodeAt() > 96 && key.charCodeAt() < 123) {
+    userInput+=key;
+  }
+  else {
+    userInput=userInput;
+  }
+  console.log(key, userInput);
+  return userInput;
+  }
 
- document.querySelector('#form').addEventListener('keyup', getUserInput);
+  function getDataUrl(userInput){
+    var url = 'https://nobel-laureate-autocomplete.herokuapp.com/' +  userInput;
+    // fetchRequest("GET", url);
+    console.log(url);
+    return url;
+  }
+
+
+function fetchRequest(method, url, cb){
+  method = form.method;
+  var xhr = new XMLHttpRequest ();
+    xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 & xhr.status === 200){
+      cb(xhr.responseText);
+    } else {
+      console.log('Waiting for response');
+    }
+  };
+xhr.open(method, url, true);
+xhr.send();
+}
