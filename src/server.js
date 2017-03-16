2900187c46daac2e2a5976a4fac962300bf1a177
Joey Scott <joannascott29@hotmail.co.uk>
2017-03-16 11:22:11 +0000

    Change port so that Heroku can recognise it Relates #23

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ var app = function(req, res){
   router(req,res);
 };
 
-var port = 4000;
+var port = process.env.PORT || 4000;
 var server = http.createServer(app).listen(port);
 console.log(`Server is listening on port: ${port}`);
