5fd62f71be58179cf34c8974c0cef5fd6080541f
Joey Scott <joannascott29@hotmail.co.uk>
2017-03-17 09:39:29 +0000

    Add getDataUrl test To test that getDataUrl returns the correct url Relates #49

--- test/tests.js
+++ test/tests.js
@@ -2,3 +2,7 @@
 // QUnit.test('1 + 1 equals 2', function(assert) {
 //   assert.equal(add(1, 1), 2, '1 + 1 should equal 2');
 // });
+
+  QUnit.test('Test that getDataUrl returns correct URL', function(assert) {
+    assert.equal(getDataUrl('barack'), 'http://localhost:4000/search/barack', 'Yay, our test works!');
+  });
